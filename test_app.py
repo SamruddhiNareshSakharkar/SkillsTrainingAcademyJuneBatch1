76b58e07292c6dd1dd7a696d0738d4e95b0b4a83
SamruddhiNareshSakharkar <samruddhisakharkar04@gmail.com>
2026-06-03 16:24:38 +0530

    ciml commit7

--- test_app.py
+++ test_app.py
@@ -23,4 +23,4 @@ def test_userDetails():
     assert response.status_code == 200
     assert response.json["name"] == "Ajit Gupta"
     assert response.json["age"] == 31
-    assert response.json["maritalStatus"] == "Married"
+    assert response.json["MaritalStatus"] == "Married"
